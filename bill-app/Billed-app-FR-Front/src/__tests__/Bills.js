de0b2f254e2b80ff3fccde0540976226dd262079
leen1515 <leventredeparis@gmail.com>
2023-07-17 16:23:10 +0200

    fix(test): l'icone doit surbriller si l'utilisateur est sur la liste des notes - ajout expect

--- bill-app/Billed-app-FR-Front/src/__tests__/Bills.js
+++ bill-app/Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,34 +2,39 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from '@testing-library/dom'
-import BillsUI from '../views/BillsUI.js'
-import { bills } from '../fixtures/bills.js'
-import { ROUTES_PATH } from '../constants/routes.js'
-import { localStorageMock } from '../__mocks__/localStorage.js'
+import {screen, waitFor} from "@testing-library/dom"
+import userEvent from "@testing-library/user-event"
+import BillsUI from "../views/BillsUI.js"
+import Bills from "../containers/Bills.js"
+import {ROUTES, ROUTES_PATH} from "../constants/routes.js"
+import {localStorageMock} from "../__mocks__/localStorage.js"
+import mockedStore from "../__mocks__/store.js"
+import {bills} from "../fixtures/bills.js"
+import router from "../app/Router.js"
 
-import router from '../app/Router.js'
-
-describe('Given I am connected as an employee', () => {
-  describe('When I am on Bills Page', () => {
-    test('Then bill icon in vertical layout should be highlighted', async () => {
+jest.mock("../app/store", () => mockedStore)
+//getBills containers/Bills.js
+describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills Page", () => {
+    test("Then bill icon in vertical layout should be highlighted", async () => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement('div')
-      root.setAttribute('id', 'root')
+      window.localStorage.setItem('user', JSON.stringify({type: 'Employee',}))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
       document.body.append(root)
       router()
-      window.onNavigate(ROUTES_PATH.Bills)
+      window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      // to-do write expect expression
+      // verifie si l'id de l'icone contient la class   
+			const isIconActivated = windowIcon.classList.contains("active-icon")
+			expect(isIconActivated).toBeTruthy()
     })
-    test('Then bills should be ordered from earliest to latest', () => {
+    //test de la méthode sort dans le fichier views/BillsUI.js
+    test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map((a) => a.innerHTML);
+      const antiChrono = (a, b) => (a < b ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
