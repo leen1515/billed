ce3cedc9ddaff37da824146f1f57c0f682a28e47
leen1515 <leventredeparis@gmail.com>
2023-07-17 16:11:44 +0200

    fix(nouvelle note): optimisation + message changé

--- bill-app/Billed-app-FR-Front/src/containers/NewBill.js
+++ bill-app/Billed-app-FR-Front/src/containers/NewBill.js
@@ -1,35 +1,33 @@
 import { ROUTES_PATH } from '../constants/routes.js'
-import Logout from './Logout.js'
+import Logout from "./Logout.js"
 
 export default class NewBill {
-  constructor ({ document, onNavigate, store, localStorage }) {
+  constructor({ document, onNavigate, store, localStorage }) {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
-    const formNewBill = this.document.querySelector('form[data-testid="form-new-bill"]')
-    formNewBill.addEventListener('submit', this.handleSubmit)
-    const file = this.document.querySelector('input[data-testid="file"]')
-    file.addEventListener('change', this.handleChangeFile)
+    const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
+    formNewBill.addEventListener("submit", this.handleSubmit)
+    const file = this.document.querySelector(`input[data-testid="file"]`)
+    file.addEventListener("change", this.handleChangeFile)
     this.fileUrl = null
     this.fileName = null
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
-
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.document.querySelector('input[data-testid="file"]').files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length - 1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem('user')).email
-    formData.append('file', file)
-    formData.append('email', email)
-    // ajout d'un message erreur et d'une condition n'autorisant que les formats : jpeg, jpg, png
+    const file = e.target.files[0]
+    const fileName = e.target.files[0].name
     const errorMessage = document.querySelector('.error-message')
-    const regexFormat = /jpeg|jpg|png$/
-    if ( regexFormat.test(e.target.value) || regexFormat.test(e.target.value) || regexFormat.test(e.target.value)) {
-      errorMessage.innerHTML = ""
+    const email = JSON.parse(localStorage.getItem("user")).email
+    // ajout d'une condition pour bloquer l'envoi de la nouvelle note
+    if(fileName.includes(".jpg") ||fileName.includes(".jpeg") || fileName.includes(".png")) {
+      e.target.setCustomValidity("")
+      errorMessage.textContent = ""
+      const formData = new FormData()
+      formData.append('file', file)
+      formData.append('email', email)
       this.store
         .bills()
         .create({
@@ -38,16 +36,18 @@ export default class NewBill {
             noContentType: true
           }
         })
-        .then(({ fileUrl, key }) => {
+        .then(({fileUrl, key}) => {
           console.log(fileUrl)
           this.billId = key
           this.fileUrl = fileUrl
           this.fileName = fileName
         }).catch(error => console.error(error))
-    }else{
-      errorMessage.innerHTML = "Vous n'avez pas choisi d'image ou le format de votre justificatif n'est pas valide. Image acceptée : JPEG, JPG, PNG"
+      } else {
+        errorMessage.textContent = "Le format de votre justificatif n'est pas valide. Image acceptée : JPEG, JPG, PNG"
+        e.target.setCustomValidity("Format invalide")  
+      }
   }
-}
+
   handleSubmit = e => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector('input[data-testid="datepicker"]').value)
@@ -64,9 +64,6 @@ export default class NewBill {
       fileUrl: this.fileUrl,
       fileName: this.fileName,
       status: 'pending'
-    }
-    if (!this.fileName){
-      return
     }
     this.updateBill(bill)
     this.onNavigate(ROUTES_PATH.Bills)
